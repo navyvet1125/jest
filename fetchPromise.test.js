26a949a6aac44120bde3b57198d94ff2e471b690
Evan Washington <enavy04@gmail.com>
2024-02-23 13:48:01 -0800

    added async/ await test

--- fetchPromise.test.js
+++ fetchPromise.test.js
@@ -2,3 +2,7 @@ const fetchPromise = require ('./fetchPromise');
 
 test('The data is "apple".', () => expect(fetchPromise()).resolves.toBe('apple'))
 // test('Fetch fails with an error', () => expect(fetchPromise()).rejects.toThrow('error'))
+test('The data is "apple".', async () => {
+    const data = await fetchPromise();
+    expect(data).toBe('apple');
+});

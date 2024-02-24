d8b2a3478489d38170a14f23820bf856d2437f72
Evan Washington <enavy04@gmail.com>
2024-02-23 18:31:09 -0800

    added spy test.  Updated fetchPromis.js and corresponding tests.

--- fetchPromise.js
+++ fetchPromise.js
@@ -1,3 +1,12 @@
-const fetchPromise = () => new Promise((resolve, reject) => setTimeout(() => resolve('apple'), 1000));
-
+// const fetchPromise = () => new Promise((resolve, reject) => setTimeout(() => resolve('apple'), 1000));
+const fetchPromise = (isComplete) => {
+    return new Promise((resolve, reject) => {
+        if (isComplete) {
+            setTimeout(() => resolve('Hi'), 1000)
+        }
+        else {
+            setTimeout(() => reject(new Error('error')), 1000)
+        }
+    })
+}
 module.exports = fetchPromise;
--- fetchPromise.test.js
+++ fetchPromise.test.js
@@ -1,8 +1,30 @@
 const fetchPromise = require ('./fetchPromise');
 
-test('The data is "apple".', () => expect(fetchPromise()).resolves.toBe('apple'))
+// test('The data is "apple".', () => expect(fetchPromise()).resolves.toBe('apple'))
 // test('Fetch fails with an error', () => expect(fetchPromise()).rejects.toThrow('error'))
-test('The data is "apple".', async () => {
-    const data = await fetchPromise();
-    expect(data).toBe('apple');
+// test('The data is "apple".', async () => {
+//     const data = await fetchPromise();
+//     expect(data).toBe('apple');
+// });
+test("fetchPromise is 'Hi'", () => expect(fetchPromise(true)).resolves.toBe("Hi"))
+
+test("fetchPromise throws", () => expect(fetchPromise(false)).rejects.toThrow("error"))
+
+
+
+test('mock implementation of a basic function', () => {
+    const mock = jest.fn(x => 42 + x);
+    expect(mock(10)).toBe(52);
+    expect(mock).toHaveBeenCalledWith(10);
 });
+test('Spying on a method of an Object', () => {
+    const video = {
+        play(){
+            return true;
+        }
+    };
+    const spy = jest.spyOn(video, 'play'); //creates a mock function copy of video.play()
+    video.play();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();  //returns implementation to original function
+})
